123eb7f00157b4a0d5b8ac190b37d6803a2cf634
Darshna <darshnakhatri989@gmail.com>
2023-06-13 00:21:14 +0530

    submitting code-Tue Jun 13 2023 00:21:10 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-
 import React from "react";
 import './../styles/App.css';
-import Tabs from "./tabs";
-
-const tabs = [{titles:"Tab 1" , contents:"Tab 1"},{titles:"Tab 2" , contents:"Tab 2"},{titles:"Tab 3" , contents:"Tab 3"}]
+import Tab from "./Tabs";
 const App = () => {
+
+  const tabs = [{titles:'Tab 1',contents:'Tab 1'},{titles:'Tab 2',contents:'Tab 2'},{titles:'Tab 3',contents:'Tab 3'}]
+
   return (
     <div>
-        <Tabs tabs={tabs}/>
+        {/* Do not remove the main div */}
+        <Tab
+          tabs={tabs}
+        ></Tab>
     </div>
   )
 }
 
-export default App;
+export default App
